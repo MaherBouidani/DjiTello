e3f34b4168f4e348f47dc27ddbb6392a90847d6f
Maher  Bouidani <maherbouidani@Mahers-MacBook-Pro.local>
2019-12-26 18:09:58 -0800

    Port config and sdk/battery level

---
+++ service.js
@@ -0,0 +1,24 @@
+const dgram = require('dgram');
+// const wait = require('waait'); 
+
+
+const PORT = 8889;
+const HOST = '192.168.10.1';
+
+const server = dgram.createSocket('udp4');
+server.bind(PORT);
+
+server.on('message', message => {
+    console.log(`${message}`);
+});
+
+function handleError(err){
+    
+    err ? console.log("Error", err): null;
+}
+
+
+
+server.send('command',0,7, PORT, HOST,handleError);
+server.send('battery?',0,8, PORT, HOST,handleError);
+
